e05946544540e9a5aba58c3e756680c5fa14a11b
Carlos Gonzalez <carlossgv@gmail.com>
2021-04-26 12:38:11 -0400

    db implemented. Supplier, Expenses models created

--- accounting/admin.py
+++ accounting/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Expense, Supplier
 
-# Register your models here.
+admin.site.register(Supplier)
+admin.site.register(Expense)
+
+
--- accounting/models.py
+++ accounting/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.db.models.deletion import CASCADE
+from django.db.models.fields import CommaSeparatedIntegerField
+
 
 # Create your models here.
+class Supplier(models.Model):
+    TYPE = (
+        ("operationFood", "Operation (Food)"),
+        ("operationLiquor", "Operation (Liquor)"),
+        ("labor", "Labor"),
+        ("service", "Service"),
+    )
+    name = models.CharField(max_length=100, unique=True)
+    supplierType = models.CharField(max_length=20, choices=TYPE)
+    isFixedExpense = models.BooleanField(default=False)
+    hasCredit = models.BooleanField(default=False)
+
+    def __str__(self) -> str:
+        return f"{self.name ({self.supplierType})}"
+
+
+class Expense(models.Model):
+    COST_CENTER = (
+        ("cash", "Cash"),
+        ("primaryAccount", "Primary Account"),
+        ("expensesAccount", "Expenses Account"),
+    )
+    supplier = models.ForeignKey(Supplier, related_name="supplier_name", on_delete=CASCADE)
+    amount = models.DecimalField(max_digits=8, decimal_places=2)
+    date = models.DateField()
+    costCenter = models.CharField(max_length=20, choices=COST_CENTER)
+    isPaid = models.BooleanField(default=True)
+    reference = models.CharField(max_length=50, blank=True)
+    comments = models.TextField(blank=True)
+
+    def __str__(self) -> str:
+        return f"{self.amount} paid to {self.supplier.name}"
