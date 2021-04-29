48c1a4260536d0f05f390bc709f3b562d1b73232
Carlos Gonzalez <carlossgv@gmail.com>
2021-04-28 21:35:36 -0400

    buttons and dropdown disabled implementation in js

--- accounting/static/accounting/js/expenses.js
+++ accounting/static/accounting/js/expenses.js
@@ -1,1 +1,38 @@
-document.addEventListener('DOMContentLoaded', function () {});
+document.addEventListener('DOMContentLoaded', function () {
+  const dropdowns = document.querySelectorAll('select');
+
+  const suppliers = document.querySelector('#id_suppliers');
+  const supplierType = document.querySelector('#id_supplier_type');
+
+  const goalsButton = document.querySelector('#getReport');
+  //   const cashButton = document.querySelector('#getCashReport');
+
+  dropdowns.forEach((element) => {
+    element.addEventListener('change', () => {
+      if (suppliers.value != '' || supplierType.value != '') {
+        goalsButton.disabled = true;
+        // cashButton.disabled = true;
+      } else {
+        goalsButton.disabled = false;
+        // cashButton.disabled = false;
+      }
+
+      // TODO: There has to be a better way to do this
+      if (suppliers.value != '') {
+        nearestInput = supplierType.closest('div').children[0];
+        nearestInput.disabled = true;
+      } else {
+        nearestInput = supplierType.closest('div').children[0];
+        nearestInput.disabled = false;
+      }
+
+      if (supplierType.value != '') {
+        nearestInput = suppliers.closest('div').children[0];
+        nearestInput.disabled = true;
+      } else {
+        nearestInput = suppliers.closest('div').children[0];
+        nearestInput.disabled = false;
+      }
+    });
+  });
+});
