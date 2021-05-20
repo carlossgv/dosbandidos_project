43b635d282e344aff1039873a8738010a77cb0ee
Carlos Gonzalez <carlossgv@gmail.com>
2021-05-20 11:07:54 -0400

    added models to admin.py

--- accounting/admin.py
+++ accounting/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Expense, Supplier
+from .models import Expense, Supplier, Income, CashLog, Metric
 
 admin.site.register(Supplier)
 admin.site.register(Expense)
-
-
+admin.site.register(Income)
+admin.site.register(CashLog)
+admin.site.register(Metric)
