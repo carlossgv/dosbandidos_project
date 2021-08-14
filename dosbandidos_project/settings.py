c5449d2d0e9cdf3fb5aa2bcd34ff8b21877238ee
Carlos Gonzalez <carlossgv@gmail.com>
2021-08-14 10:23:40 -0400

    added localhost to allowed hosts

--- dosbandidos_project/settings.py
+++ dosbandidos_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get("DOSBANDIDOS_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["192.168.1.112", "127.0.0.1", "192.168.1.7"]
+ALLOWED_HOSTS = ["192.168.1.112", "127.0.0.1", "192.168.1.7", "localhost"]
 
 
 # Application definition
