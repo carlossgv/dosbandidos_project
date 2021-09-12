8a35a9d5fdc4f1e051bceb29e80affba6a265f93
Carlos Gonzalez <carlossgv@gmail.com>
2021-09-11 21:04:18 -0300

    Delete unused columns from accounting_supplier (hasCredit, isFixedExpense)

--- accounting/models.py
+++ accounting/models.py
@@ -15,8 +15,6 @@ class Supplier(models.Model):
     )
     name = models.CharField(max_length=100, unique=True)
     supplierType = models.CharField(max_length=20, choices=TYPE)
-    isFixedExpense = models.BooleanField(default=False)
-    hasCredit = models.BooleanField(default=False)
     comments = models.TextField(blank=True)
 
     class Meta:
