a5b4d1c794d0eee4c0d8e1047e5fa32e3a07a6cd
Carlos Gonzalez <carlossgv@gmail.com>
2021-10-11 11:20:10 -0300

    adapting to heroku

--- dosbandidos_project/settings.py
+++ dosbandidos_project/settings.py
@@ -80,14 +80,21 @@ WSGI_APPLICATION = "dosbandidos_project.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
-#     }
-# }
 
-DATABASES = {'default': dj_database_url.config()}
+if os.environ.get("DOS_BANDIDOS_IS_DEBUG") == 'True':
+
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
+
+else:
+    DATABASES = {
+        'default': dj_database_url.config()
+    }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
