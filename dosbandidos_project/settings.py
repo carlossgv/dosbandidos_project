57c569e5c9135f86a02afe7a468bba6170fb04f8
Carlos Gonzalez <carlossgv@gmail.com>
2021-10-11 13:05:38 -0300

    adapting to heroku

--- dosbandidos_project/settings.py
+++ dosbandidos_project/settings.py
@@ -81,38 +81,38 @@ WSGI_APPLICATION = "dosbandidos_project.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+#
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+print(DEBUG)
+if DEBUG:
+
+    # DATABASES = {
+    #     "default": {
+    #         "ENGINE": "django.db.backends.sqlite3",
+    #         "NAME": BASE_DIR / "db.sqlite3",
+    #     }
+    # }
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'dosbandidos_local',
+            'USER': 'carlossgv',
+            'PASSWORD': 'postgres',
+            'HOST': 'localhost',
+            'PORT': '5432',
+        }
     }
-}
 
-# print(DEBUG)
-# if DEBUG:
-#
-#     # DATABASES = {
-#     #     "default": {
-#     #         "ENGINE": "django.db.backends.sqlite3",
-#     #         "NAME": BASE_DIR / "db.sqlite3",
-#     #     }
-#     # }
-#     DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#             'NAME': 'dosbandidos_local',
-#             'USER': 'carlossgv',
-#             'PASSWORD': 'postgres',
-#             'HOST': 'localhost',
-#             'PORT': '5432',
-#         }
-#     }
-#
-# else:
-#     DATABASES = {
-#         'default': dj_database_url.config()
-#     }
+else:
+    DATABASES = {
+        'default': dj_database_url.config()
+    }
 
 
 # Password validation
