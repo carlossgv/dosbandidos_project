c9af9d3d085ad01b4a7400ec8160d65bbefd23d1
Carlos Gonzalez <carlossgv@gmail.com>
2021-10-23 15:25:09 -0300

    fixing static error in heroku

--- dosbandidos_project/settings.py
+++ dosbandidos_project/settings.py
@@ -151,6 +151,7 @@ USE_TZ = True
 STATIC_URL = "/static/"
 # STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'staticfiles'))
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+print(STATICFILES_DIRS)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
