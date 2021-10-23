6999a9bf90765d4999abacaff8f42dca3a55f016
Carlos Gonzalez <carlossgv@gmail.com>
2021-10-23 13:57:20 -0300

    added Rule model to admin.py

--- accounting/admin.py
+++ accounting/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Expense, Supplier, Income, CashLog, Metric
+from .models import Expense, Supplier, Income, CashLog, Metric, Rule
 
 admin.site.register(Supplier)
 admin.site.register(Expense)
 admin.site.register(Income)
 admin.site.register(CashLog)
 admin.site.register(Metric)
+admin.site.register(Rule)
