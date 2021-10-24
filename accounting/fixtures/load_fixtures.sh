d30b2f7e1c9a178a393742586e9e6efbea15597d
Carlos Gonzalez <carlossgv@gmail.com>
2021-10-24 19:57:02 -0300

    fixed load fixtures path

--- accounting/fixtures/load_fixtures.sh
+++ accounting/fixtures/load_fixtures.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-./manage.py loaddata ./Supplier.json
-./manage.py loaddata ./Rule.json
-./manage.py loaddata ./User.json
-./manage.py loaddata ./Expense.json
-./manage.py loaddata ./Income.json
-./manage.py loaddata ./Cashlog.json
-./manage.py loaddata ./Metric.json
+../../manage.py loaddata ./Supplier.json
+../../manage.py loaddata ./Rule.json
+../../manage.py loaddata ./User.json
+../../manage.py loaddata ./Expense.json
+../../manage.py loaddata ./Income.json
+../../manage.py loaddata ./Cashlog.json
+../../manage.py loaddata ./Metric.json
