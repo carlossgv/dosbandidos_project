69a25dd167b0c4a1c65a659414b5a49231cb0a09
Carlos Gonzalez <carlossgv@gmail.com>
2021-10-24 20:08:40 -0300

    fixed again

--- accounting/fixtures/load_fixtures.sh
+++ accounting/fixtures/load_fixtures.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-./manage.py loaddata accounting/fixtures/Supplier.json
-./manage.py loaddata accounting/fixtures/Rule.json
-./manage.py loaddata accounting/fixtures/User.json
-./manage.py loaddata accounting/fixtures/Expense.json
-./manage.py loaddata accounting/fixtures/Income.json
-./manage.py loaddata accounting/fixtures/Cashlog.json
-./manage.py loaddata accounting/fixtures/Metric.json
+./manage.py loaddata ./accounting/fixtures/Supplier.json
+./manage.py loaddata ./accounting/fixtures/Rule.json
+./manage.py loaddata ./accounting/fixtures/User.json
+./manage.py loaddata ./accounting/fixtures/Expense.json
+./manage.py loaddata ./accounting/fixtures/Income.json
+./manage.py loaddata ./accounting/fixtures/Cashlog.json
+./manage.py loaddata ./accounting/fixtures/Metric.json
