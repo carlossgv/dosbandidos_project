f2f75e37af60da08ab69ce8480092d1f14025930
Carlos Gonzalez <carlossgv@gmail.com>
2021-11-13 22:01:55 -0300

    testing environ

--- accounting/utils_api_clover.py
+++ accounting/utils_api_clover.py
@@ -5,13 +5,20 @@ import pytz as pytz
 
 
 def get_orders_clover(merchant_id, initial_date, finish_date):
-    initial_date = transform_to_epoch(initial_date, 'initial')
-    finish_date = transform_to_epoch(finish_date, 'finish')
+    initial_date = transform_to_epoch(initial_date, "initial")
+    finish_date = transform_to_epoch(finish_date, "finish")
 
-    headers = {'accept': 'application/json', 'authorization': f'Bearer {os.environ.get("DOSBANDIDOS_CLOVER_API")}'}
+    print(os.environ.get("DOSBANDIDOS_CLOVER_API"))
+
+    headers = {
+        "accept": "application/json",
+        "authorization": f'Bearer {os.environ.get("DOSBANDIDOS_CLOVER_API")}',
+    }
     response = requests.get(
-        f'https://api.clover.com/v3/merchants/{merchant_id}/orders?filter=createdTime>{initial_date}'
-        f'&filter=createdTime<{finish_date}&expand=payments&expand=serviceCharge&limit=500', headers=headers)
+        f"https://api.clover.com/v3/merchants/{merchant_id}/orders?filter=createdTime>{initial_date}"
+        f"&filter=createdTime<{finish_date}&expand=payments&expand=serviceCharge&limit=500",
+        headers=headers,
+    )
 
     orders = response.json()
 
@@ -19,9 +26,14 @@ def get_orders_clover(merchant_id, initial_date, finish_date):
 
 
 def get_payment_details(merchant_id, payment_id):
-    headers = {'accept': 'application/json', 'authorization': f'Bearer {os.environ.get("DOSBANDIDOS_CLOVER_API")}'}
+    headers = {
+        "accept": "application/json",
+        "authorization": f'Bearer {os.environ.get("DOSBANDIDOS_CLOVER_API")}',
+    }
     response = requests.get(
-        f'https://api.clover.com/v3/merchants/{merchant_id}/payments/{payment_id}?expand=taxRates', headers=headers)
+        f"https://api.clover.com/v3/merchants/{merchant_id}/payments/{payment_id}?expand=taxRates",
+        headers=headers,
+    )
 
     payment_details = response.json()
 
@@ -29,9 +41,14 @@ def get_payment_details(merchant_id, payment_id):
 
 
 def get_refund_details(merchant_id, refund_id):
-    headers = {'accept': 'application/json', 'authorization': f'Bearer {os.environ.get("DOSBANDIDOS_CLOVER_API")}'}
+    headers = {
+        "accept": "application/json",
+        "authorization": f'Bearer {os.environ.get("DOSBANDIDOS_CLOVER_API")}',
+    }
     response = requests.get(
-        f'https://api.clover.com/v3/merchants/{merchant_id}/refunds/{refund_id}?expand=serviceCharge', headers=headers)
+        f"https://api.clover.com/v3/merchants/{merchant_id}/refunds/{refund_id}?expand=serviceCharge",
+        headers=headers,
+    )
 
     refund_details = response.json()
 
@@ -39,13 +56,18 @@ def get_refund_details(merchant_id, refund_id):
 
 
 def get_refunds_list(merchant_id, initial_date, finish_date):
-    initial_date = transform_to_epoch(initial_date, 'initial')
-    finish_date = transform_to_epoch(finish_date, 'finish')
+    initial_date = transform_to_epoch(initial_date, "initial")
+    finish_date = transform_to_epoch(finish_date, "finish")
 
-    headers = {'accept': 'application/json', 'authorization': f'Bearer {os.environ.get("DOSBANDIDOS_CLOVER_API")}'}
+    headers = {
+        "accept": "application/json",
+        "authorization": f'Bearer {os.environ.get("DOSBANDIDOS_CLOVER_API")}',
+    }
     response = requests.get(
-        f'https://api.clover.com/v3/merchants/{merchant_id}/refunds?filter=createdTime>{initial_date}'
-        f'&filter=createdTime<{finish_date}&limit=500', headers=headers)
+        f"https://api.clover.com/v3/merchants/{merchant_id}/refunds?filter=createdTime>{initial_date}"
+        f"&filter=createdTime<{finish_date}&limit=500",
+        headers=headers,
+    )
 
     refunds = response.json()
 
@@ -53,8 +75,8 @@ def get_refunds_list(merchant_id, initial_date, finish_date):
 
 
 def daily_cash_data_clover(merchant_id, date):
-    orders = get_orders_clover(merchant_id, date, date)['elements']
-    refunds = get_refunds_list(merchant_id, date, date)['elements']
+    orders = get_orders_clover(merchant_id, date, date)["elements"]
+    refunds = get_refunds_list(merchant_id, date, date)["elements"]
 
     # Cash Tender ID: D8ER2CY0D5NX8
 
@@ -63,55 +85,60 @@ def daily_cash_data_clover(merchant_id, date):
     card_tips = 0
 
     for order in orders:
-        for payment in order['payments']['elements']:
+        for payment in order["payments"]["elements"]:
 
-            if 'tipAmount' in payment:
-                card_tips += payment['tipAmount'] / 100
+            if "tipAmount" in payment:
+                card_tips += payment["tipAmount"] / 100
 
-            if 'serviceCharge' in order:
-                service_charge_percentage = order['serviceCharge']['percentage']
-                payment_details = get_payment_details(merchant_id, payment['id'])
+            if "serviceCharge" in order:
+                service_charge_percentage = order["serviceCharge"]["percentage"]
+                payment_details = get_payment_details(merchant_id, payment["id"])
 
-                for tax_rate in payment_details['taxRates']['elements']:
-                    taxable_amount = tax_rate['taxableAmount'] / 100
+                for tax_rate in payment_details["taxRates"]["elements"]:
+                    taxable_amount = tax_rate["taxableAmount"] / 100
 
                     card_auto += taxable_amount * service_charge_percentage / 100
 
-            if payment['tender']['id'] == 'D8ER2CY0D5NX8':
-                cash_sales += payment['amount'] / 100
+            if payment["tender"]["id"] == "D8ER2CY0D5NX8":
+                cash_sales += payment["amount"] / 100
 
     for refund in refunds:
-        if 'tipAmount' in refund:
-            card_tips -= refund['tipAmount'] / 100
+        if "tipAmount" in refund:
+            card_tips -= refund["tipAmount"] / 100
 
-        if 'serviceCharge' in refund:
-            service_charge_percentage = refund['serviceCharge']['percentage']
-            refund_details = get_refund_details(merchant_id, refund['id'])
+        if "serviceCharge" in refund:
+            service_charge_percentage = refund["serviceCharge"]["percentage"]
+            refund_details = get_refund_details(merchant_id, refund["id"])
 
-            for tax_rate in refund_details['taxRates']['elements']:
-                taxable_amount = tax_rate['taxableAmount'] / 100
+            for tax_rate in refund_details["taxRates"]["elements"]:
+                taxable_amount = tax_rate["taxableAmount"] / 100
 
                 card_auto -= taxable_amount * service_charge_percentage / 100
 
-        if refund['payment']['tender']['id'] == 'D8ER2CY0D5NX8':
-            cash_sales -= refund['amount'] / 100
+        if refund["payment"]["tender"]["id"] == "D8ER2CY0D5NX8":
+            cash_sales -= refund["amount"] / 100
 
-    return {'cash_sales': cash_sales, 'card_auto': card_auto, 'card_tips': card_tips}
+    return {"cash_sales": cash_sales, "card_auto": card_auto, "card_tips": card_tips}
 
 
-def transform_to_epoch(date, date_type, location='US/Central'):
-    offset = pytz.timezone(location).localize(datetime.datetime(date.year, date.month, date.day)).strftime('%z')
+def transform_to_epoch(date, date_type, location="US/Central"):
+    offset = (
+        pytz.timezone(location)
+        .localize(datetime.datetime(date.year, date.month, date.day))
+        .strftime("%z")
+    )
     offset = int(offset) * -1 / 100
 
-    if date_type == 'initial':
+    if date_type == "initial":
         hour = 0
         minute = 0
-    elif date_type == 'finish':
+    elif date_type == "finish":
         hour = 23
         minute = 59
 
-    date = datetime.datetime(date.year, date.month, date.day, hour, minute, tzinfo=datetime.timezone.utc) + \
-           datetime.timedelta(hours=offset)
+    date = datetime.datetime(
+        date.year, date.month, date.day, hour, minute, tzinfo=datetime.timezone.utc
+    ) + datetime.timedelta(hours=offset)
 
     date = int(date.timestamp()) * 1000
 
