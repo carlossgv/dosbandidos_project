54efea65f65d3b1d4e7ecd8455d6acce3a3bdf50
Carlos Gonzalez <carlossgv@gmail.com>
2021-11-20 16:27:44 -0300

    dropdown navbar implemented

--- accounting/static/accounting/js/main.js
+++ accounting/static/accounting/js/main.js
@@ -3,4 +3,8 @@ document.addEventListener('DOMContentLoaded', function () {
   var instances = M.FormSelect.init(elems, {});
   var elems = document.querySelectorAll('.sidenav');
   var instances = M.Sidenav.init(elems, {});
+  var dropdowns = document.querySelectorAll('.dropdown-trigger')
+  for (var i = 0; i < dropdowns.length; i++){
+      M.Dropdown.init(dropdowns[i]);
+  }
 });
