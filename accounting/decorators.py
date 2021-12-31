df798d6c5d90f78b2b041d927a17bb0576f13a0a
Carlos Gonzalez <carlossgv@gmail.com>
2021-12-31 00:21:12 -0300

    created decorator and filter for managers

--- accounting/decorators.py
+++ accounting/decorators.py
@@ -2,23 +2,27 @@ from functools import wraps
 from django.http import HttpResponseRedirect
 
 # Decorator to check if user is admin
+# If not, redirect to home page
 def admins_only(function):
     @wraps(function)
     def wrap(request, *args, **kwargs):
         user_groups = request.user.groups.all()
 
-        if 'read_only_users' in [group.name for group in user_groups]:
+        if not request.user.is_staff:
             return HttpResponseRedirect(request.META.get('HTTP_REFERER', '/'))
         else:
             return function(request, *args, **kwargs)
     return wrap
 
+# Decorator to check if user is a manager
+# If not, redirect to home page
+def managers_only(function):
+    @wraps(function)
+    def wrap(request, *args, **kwargs):
+        user_groups = request.user.groups.all()
 
-def is_user_admin(user):
-    is_admin = True
-    user_groups = user.groups.all()
-
-    if 'read_only_users' in [group.name for group in user_groups]:
-        is_admin = False
-
-    return is_admin
+        if 'managers' in [group.name for group in user_groups]:
+            return HttpResponseRedirect(request.META.get('HTTP_REFERER', '/'))
+        else:
+            return function(request, *args, **kwargs)
+    return wrap
