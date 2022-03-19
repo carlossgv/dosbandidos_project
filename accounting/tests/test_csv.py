7dd3402605e657ff23cb29110f66bc147c180dff
Carlos Gonzalez <carlossgv@gmail.com>
2022-03-19 18:56:42 -0300

    fixed imports in tests

--- accounting/tests/test_csv.py
+++ accounting/tests/test_csv.py
@@ -1,7 +1,12 @@
 from django.test import TestCase
 
 from .script_test_database import load_test_database
-from accounting.utils_csv_handling import csv_create_expense, format_date, load_csv_expenses, load_csv_incomes
+from accounting.utils.utils_csv_handling import (
+    csv_create_expense,
+    format_date,
+    load_csv_expenses,
+    load_csv_incomes,
+)
 from accounting.models import Expense, Income, Rule, Supplier
 
 
@@ -18,68 +23,88 @@ class CsvReading(TestCase):
         Rule.objects.all().delete()
 
     def test_transform_date_to_django_format(self):
-        date = '8/6/2021'
+        date = "8/6/2021"
 
         formatted_date = format_date(date)
 
-        self.assertEqual(formatted_date, '2021-08-06')
+        self.assertEqual(formatted_date, "2021-08-06")
 
     def test_create_expense_by_row_if_no_rule(self):
-        row = ['XXXXXX4583', '8/2/2021', '110134',
-               'CHECK -  -', '615.58', '', 'Posted', '']
+        row = [
+            "XXXXXX4583",
+            "8/2/2021",
+            "110134",
+            "CHECK -  -",
+            "615.58",
+            "",
+            "Posted",
+            "",
+        ]
 
-        cost_center = 'primaryAccount'
+        cost_center = "primaryAccount"
 
         csv_create_expense(row, 1, cost_center)
 
-        expense = Expense.objects.all().order_by('-id')[0]
+        expense = Expense.objects.all().order_by("-id")[0]
 
         self.assertEqual(expense.supplier.pk, 100)
         self.assertEqual(float(expense.amount), 615.58)
         self.assertEqual(expense.cost_center, cost_center)
 
     def test_create_expense_by_row_if_description_contains_a_rule(self):
-        row = ['XXXXXX4583', '8/6/2021', '', 'ACH Debit - ACH Bill.com   Superior Linen - Dos Bandidos - Broken',
-               '97.50', '', 'Posted', '']
-        cost_center = 'primaryAccount'
+        row = [
+            "XXXXXX4583",
+            "8/6/2021",
+            "",
+            "ACH Debit - ACH Bill.com   Superior Linen - Dos Bandidos - Broken",
+            "97.50",
+            "",
+            "Posted",
+            "",
+        ]
+        cost_center = "primaryAccount"
 
         csv_create_expense(row, 1, cost_center)
 
-        expense = Expense.objects.all().order_by('-id')[0]
+        expense = Expense.objects.all().order_by("-id")[0]
 
         self.assertEqual(expense.supplier.pk, 2)
         self.assertEqual(expense.amount, 97.50)
-        self.assertEqual(expense.cost_center, 'primaryAccount')
+        self.assertEqual(expense.cost_center, "primaryAccount")
 
     def test_load_expenses_to_db(self):
         filepath = "./tests_data/AccountHistory.csv"
         delimiter = ","
         cost_center = "primaryAccount"
-        load_csv_expenses(filepath, delimiter, '1', cost_center)
+        load_csv_expenses(filepath, delimiter, "1", cost_center)
 
         clover_id = Supplier.objects.get(pk=4)
         test_expense = Expense.objects.get(supplier=clover_id)
 
-        self.assertEqual(str(test_expense.amount), '89.85')
-        self.assertEqual(str(test_expense.date), '2021-09-10')
-        self.assertEqual(str(test_expense.cost_center), 'primaryAccount')
-        self.assertEqual(str(test_expense.supplier_id), '4')
-        self.assertEqual(str(test_expense.restaurant_id), '1')
-        self.assertEqual(str(test_expense.reference), '')
-        self.assertEqual(str(test_expense.comments),
-                         'ACH Debit - ACH CLOVER APP CLOVER APP MRKT - DOS BANDIDOS BA LLC')
+        self.assertEqual(str(test_expense.amount), "89.85")
+        self.assertEqual(str(test_expense.date), "2021-09-10")
+        self.assertEqual(str(test_expense.cost_center), "primaryAccount")
+        self.assertEqual(str(test_expense.supplier_id), "4")
+        self.assertEqual(str(test_expense.restaurant_id), "1")
+        self.assertEqual(str(test_expense.reference), "")
+        self.assertEqual(
+            str(test_expense.comments),
+            "ACH Debit - ACH CLOVER APP CLOVER APP MRKT - DOS BANDIDOS BA LLC",
+        )
 
     def test_load_incomes_to_db(self):
         filepath = "./tests_data/AccountHistory.csv"
         delimiter = ","
-        load_csv_incomes(filepath, delimiter, '1')
+        load_csv_incomes(filepath, delimiter, "1")
 
         doordash_id = Supplier.objects.get(pk=5)
         text_income = Income.objects.get(supplier=doordash_id)
 
-        self.assertEqual(str(text_income.amount), '1021.26')
-        self.assertEqual(str(text_income.date), '2021-09-10')
-        self.assertEqual(str(text_income.comments),
-                         'ACH Credit - ACH 210 S Memo DoorDash, Inc. - DOS BANDIDOS BA LLC')
-        self.assertEqual(str(text_income.supplier_id), '5')
-        self.assertEqual(str(text_income.restaurant_id), '1')
+        self.assertEqual(str(text_income.amount), "1021.26")
+        self.assertEqual(str(text_income.date), "2021-09-10")
+        self.assertEqual(
+            str(text_income.comments),
+            "ACH Credit - ACH 210 S Memo DoorDash, Inc. - DOS BANDIDOS BA LLC",
+        )
+        self.assertEqual(str(text_income.supplier_id), "5")
+        self.assertEqual(str(text_income.restaurant_id), "1")
--- accounting/tests/test_edit_expenses.py
+++ accounting/tests/test_edit_expenses.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 
 from .script_test_database import load_test_database
-from accounting.utils_edit_expenses import get_expenses_by_date
+from accounting.utils.utils_edit_expenses import get_expenses_by_date
 from accounting.models import Expense, Income, Supplier, Rule
 
 
@@ -18,18 +18,18 @@ class EditExpenses(TestCase):
         Rule.objects.all().delete()
 
     def test_get_all_expenses_by_date(self):
-        initial_date = '2021-05-01'
-        finish_date = '2021-06-08'
+        initial_date = "2021-05-01"
+        finish_date = "2021-06-08"
         restaurant_id = 1
-        expenses = get_expenses_by_date(
-            initial_date, finish_date, restaurant_id)
+        expenses = get_expenses_by_date(initial_date, finish_date, restaurant_id)
         self.assertEqual(len(expenses), 4)
 
     def test_get_all_expenses_by_date_and_supplier(self):
-        initial_date = '2021-05-01'
-        finish_date = '2021-06-08'
+        initial_date = "2021-05-01"
+        finish_date = "2021-06-08"
         restaurant_id = 1
         supplier_id = 3
         expenses = get_expenses_by_date(
-            initial_date, finish_date, restaurant_id, supplier_id)
+            initial_date, finish_date, restaurant_id, supplier_id
+        )
         self.assertEqual(len(expenses), 1)
