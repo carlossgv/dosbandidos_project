db3f6eff322c14e13af7ef2fbe98c5fcad2c6b89
Carlos <carlossgv@gmail.com>
2024-01-06 16:49:47 +0000

    fix: session name tmux script

--- tmux-dosbandidos-script.sh
+++ tmux-dosbandidos-script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-SESSION="Dos Bandidos"
+SESSION="dos-bandidos"
 SESSIONEXISTS=$(tmux list-sessions | grep -w "$SESSION")
 
 if [ "$SESSIONEXISTS" = "" ]
@@ -8,17 +8,17 @@ then
 
   tmux new-session -d -s "$SESSION" -d -x "$(tput cols)" -y "$(tput lines)"
 
-  tmux rename-window -t 0 'Main'
+  tmux rename-window -t 1 'Main'
+  tmux splitw -h -p 30
   tmux send-keys -t 'Main' 'cd ~/projects/dosbandidos_project' C-m
-  tmux send-keys -t 'Main' 'source dosbandidos_env/bin/activate' C-m
+  tmux send-keys -t 'Main' 'source ./envdosbandidos/bin/activate' C-m
   tmux send-keys -t 'Main' 'code .' C-m
   tmux send-keys -t 'Main' 'clear' C-m
-  tmux splitw -h -p 30
+  tmux send-keys -t 'Main' 'python3 manage.py runserver 0.0.0.0:8000' C-m
 
-  tmux send-keys -t 'Main' 'python3 manage.py runserver' C-m
   tmux select-pane -t 0
 
 fi
 
-tmux attach-session -t "$SESSION":0
+tmux attach-session -t "$SESSION":1
 
